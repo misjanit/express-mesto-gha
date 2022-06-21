a7649a8b3cfe67a29e78a404e8af1d8f01dc433d
Андрей Лиханов <mogueta63@gmail.com>
2022-06-22 00:06:39 +0500

    test

--- routes/cards.js
+++ routes/cards.js
@@ -2,15 +2,15 @@ const router = require('express').Router();
 const { getCards, createCard, deleteCard, likeCard, dislikeCard } = require('../controllers/cards');
 // GET-запрос возвращает все карточки из базы данных
 // POST-запрос создает новую карточку по переданным параметрам.
-router.get('/cards', getCards);
-router.post('/cards', createCard);
+router.get('/', getCards);
+router.post('/', createCard);
 
 // DELETE-запрос удаляет карточку по _id
-router.delete('/cards/:cardId', deleteCard);
+router.delete('/:cardId', deleteCard);
 
 // PUT-запрос добавляет лайк карточке.
 // DELETE-запрос удаляет лайк с карточки.
-router.put('/cards/:cardId/likes', likeCard);
-router.delete('/cards/:cardId/likes', dislikeCard);
+router.put('/:cardId/likes', likeCard);
+router.delete('/:cardId/likes', dislikeCard);
 
 module.exports = router;
